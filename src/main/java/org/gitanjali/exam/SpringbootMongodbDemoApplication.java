5d0d7493c109690e2057473c770ab1763a91a3d4
Srijon <srijondas@gmail.com>
2020-12-27 02:24:48 -0800

    audit logging changes

--- src/main/java/org/gitanjali/exam/SpringbootMongodbDemoApplication.java
+++ src/main/java/org/gitanjali/exam/SpringbootMongodbDemoApplication.java
@@ -1,10 +1,23 @@
 package org.gitanjali.exam;
 
+import org.gitanjali.exam.model.EntityAuditing;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.data.mongodb.config.EnableMongoAuditing;
+import org.gitanjali.*;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {"org.gitanjali.*"})
+@EnableMongoAuditing
 public class SpringbootMongodbDemoApplication {
+
+    @Bean
+    public AuditorAware<String> auditorAware(){
+        return new EntityAuditing();
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(SpringbootMongodbDemoApplication.class, args);
