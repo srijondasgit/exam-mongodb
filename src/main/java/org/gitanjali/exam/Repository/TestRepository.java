9da9ea89a991f58fb4f3e02b2a73bdde834af773
sudhir <sudhir.chappidi@cubic.com>
2021-01-15 22:09:49 +0530

    Added Javers on repo

--- src/main/java/org/gitanjali/exam/Repository/TestRepository.java
+++ src/main/java/org/gitanjali/exam/Repository/TestRepository.java
@@ -1,11 +1,10 @@
 package org.gitanjali.exam.Repository;
 
 import org.gitanjali.exam.Entity.Test;
-import org.gitanjali.exam.Entity.User;
+import org.javers.spring.annotation.JaversSpringDataAuditable;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
-import java.util.List;
-
+@JaversSpringDataAuditable
 public interface TestRepository extends MongoRepository<Test,String> {
 
     Test findByIdEquals(String id);
