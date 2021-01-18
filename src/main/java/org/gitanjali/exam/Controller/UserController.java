a582daeced7b3769cb29551baa4e132972c987d2
sudhir <sudhir.chappidi@cubic.com>
2021-01-18 15:27:55 +0530

    Api to get audit log for user

--- src/main/java/org/gitanjali/exam/Controller/UserController.java
+++ src/main/java/org/gitanjali/exam/Controller/UserController.java
@@ -1,21 +1,25 @@
 package org.gitanjali.exam.Controller;
 
 
+import java.util.List;
+
+import org.apache.commons.lang3.StringUtils;
 import org.gitanjali.exam.Entity.AuthRequest;
 import org.gitanjali.exam.Entity.Test;
 import org.gitanjali.exam.Repository.TestRepository;
 import org.gitanjali.exam.Util.JwtUtil;
-import org.javers.core.Changes;
 import org.javers.core.Javers;
-import org.javers.core.json.JsonConverter;
-import org.javers.core.metamodel.object.CdoSnapshot;
 import org.javers.repository.jql.QueryBuilder;
+import org.javers.shadow.Shadow;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/user")
@@ -54,28 +58,17 @@ public class UserController {
         return jwtUtil.generateToken(authRequest.getUserName());
     }
 
-    @RequestMapping("/test")
-    public String getTestChanges() {
-        QueryBuilder jqlQuery = QueryBuilder.byClass(Test.class)
-                .withNewObjectChanges();
-
-        Changes changes = javers.findChanges(jqlQuery.build());
-
-        return "<pre>" + changes.prettyPrint() + "</pre>";
+    @GetMapping("/audit/test")
+    public String getTestChanges(@RequestParam(required = false) String user) {
+    	QueryBuilder query = QueryBuilder.byClass(Test.class);
+    	if(StringUtils.isNotBlank(user)) {
+    		query.byAuthor(user);
+    	}
+    	query.limit(10);
+    	query.withChildValueObjects();
+    	
+        List<Shadow<Test>> shadows = javers.findShadows(query.build());
+        return javers.getJsonConverter().toJson(shadows);
     }
-
-    @RequestMapping("/test/{login}/snapshots")
-    public String getTestSnapshots(@PathVariable String login) {
-        QueryBuilder jqlQuery = QueryBuilder.byInstanceId(login, Test.class);
-
-        List<CdoSnapshot> changes = javers.findSnapshots(jqlQuery.build());
-
-        changes.sort((o1, o2) -> -1 * o1.getCommitMetadata().getCommitDate().compareTo(o2.getCommitMetadata().getCommitDate()));
-
-        JsonConverter jsonConverter = javers.getJsonConverter();
-
-        return jsonConverter.toJson(changes);
-    }
-
 
 }
